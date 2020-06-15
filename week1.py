4aacec89d8bf0314998e398960bb145a87ce88b0
BillMark98 <hupanweibill@gmail.com>
2020-06-15 21:58:18 +0200

    exercise week1

--- week1.py
+++ week1.py
@@ -1,13 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 def smallFunc(m):
-    return 2/3 * float(m) * (m - 1)/(m+2) - 2 * np.log(m + 2)
-M = list(range(10))
+    return 1/6 * float(m) * (m - 1)/(m+2) - 2 * np.log(m + 2)
+M = list(range(48))
 print(M)
 fM = list(map(smallFunc, M))
 print(fM)
 plt.plot(M,fM)
-plt.xticks(M)
+plt.xticks(M[::2])
 plt.xlabel('M')
 plt.ylabel('f(M)')
 plt.title('plot of f(M)')
