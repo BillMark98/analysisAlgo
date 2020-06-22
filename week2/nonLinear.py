b1a23bdeb6da00d81a72da080a748e6540a62f14
BillMark98 <hupanweibill@gmail.com>
2020-06-22 18:40:44 +0200

    modify week2 homework and week1 dir

--- week2/nonLinear.py
+++ week2/nonLinear.py
@@ -3,7 +3,10 @@ def myF(an):
     return an * (1 - an)
 def myF2(an):
     return math.sin(an)
-
+def myEx39(a0,N):
+    sum1 = (0.5 * (a0 + math.sqrt(a0**2 - 4)))**(2**N)
+    sum2 = (0.5 * (a0 - math.sqrt(a0**2 - 4)))**(2**N)
+    return sum1 + sum2
 
 def ex21(N):
     a0 = 1/2
@@ -47,4 +50,38 @@ def ex22(N):
     for i in range(1,N):
         print("{0:3d} & {1:6.4f}\\\\".format(i,arr[i]*math.sqrt(i)/math.sqrt(3)))
 
-ex22(22)
+def ex39(a0, N):
+    arr = list()
+    arr.append(a0)
+    for i in range(0,N):
+        print(i,end = "    ")
+    print()
+    print(a0,end = "    ")
+    for i in range(1,N):
+        a = myEx39(a0,i)
+        arr.append(a)
+        print(a, end = "    ")
+    print()
+
+    print(arr)
+
+    # # for latex
+    for i in range(1,N):
+        print("{0:3d} & {1:6.4f}\\\\".format(i,arr[i]))
+
+
+def problem(N):
+    a0 = a1 = a2 = 0
+    An = [None] * (N + 1)
+    Bn = [None] * (N + 1)
+    An[0] = An[1] = An[2] = 0
+    Bn[0] = Bn[1] = Bn[2] = 0
+    for i in range(3, N+1):
+        An[i] = (i-3)/i * An[i-1] + 1
+        Bn[i] = (i+1)/4
+    if (N < 100):
+        for i in range(3,N + 1):
+            print("{0:3d} & {1:6.4f} & {2:6.4f}\\\\".format(i,An[i],Bn[i]))
+    else:
+        print(An[N], Bn[N])
+problem(999)
